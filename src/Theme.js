bab0f04dfdc2c61a08726dbdf80c51b2e92552d0
isabelle-np <your_email@example.com>
2019-12-26 13:47:47 -0500

    Cleaned up files

---
+++ src/Theme.js
@@ -0,0 +1,9 @@
+export const TEAL = '#1c5c68';
+export const LIGHTBLUE = '#66ced1';
+export const FADEDBLUE = '#c6e9f0';
+export const BLACK = "#000000";
+export const GREY = "#414141";
+
+export default {
+
+};
---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+//import {TEAL, LIGHTBLUE, FADEDBLUE} from './the';
+
+
+const Button = styled.button`
+    background: #c6e9f0;
+    font-size: 1rem;
+    height: 1.875rem;
+    color: TEAL;
+    text-transform: uppercase;
+    border: none;
+    border-radius: .125rem;
+    padding: 0 1rem;
+    font-weight: 700;
+    margin: 1rem;
+    transition: 0.3s;
+`;
+
+export default Button;
